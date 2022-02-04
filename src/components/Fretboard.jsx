f5682697c0b523912982189573560f8d51a0e0a4
malediction99@gmail.com <malediction99@gmail.com>
2022-02-04 15:22:58 +1100

    progress

--- src/components/Fretboard.jsx
+++ src/components/Fretboard.jsx
@@ -7,19 +7,20 @@ function Fretboard() {
     const natural = "\u266E"
     const ChromaticScale = [[0, 'C'], [1, 'C'+sharp + '/D'+flat], [2, 'D'], [3, 'D'+sharp+'/E'+flat], [4, 'E'], [5, 'F'], [6, 'F'+ sharp + '/G'+flat], [7, 'G'] ,
     [8, 'G'+ sharp + '/A' + flat], [9, 'A'], [10, 'A' + sharp + '/B'+flat], [11, 'B']];
-    let notes = [9, 11, 1, 2, 4, 6, 8, 9]
+
+    let highlightedNotes = output_scale(5,0)
+
     function output_scale(tonic, category) {
         let root = ChromaticScale[tonic]
         //  Ionian - Dorian - Phrygian - Lydian - Mixolydian - Aeolian - Locrian - Harmonic Minor - Asc. Melodic Minor
         const modes = [[2, 2, 1, 2, 2, 2, 1], [2, 1, 2, 2, 2, 1, 2], [1, 2, 2, 2, 1, 2, 2], [2, 2, 2, 1, 2, 2, 1], 
         [2, 2, 1, 2, 2, 1, 2], [2, 1, 2, 2, 1, 2, 2], [1, 2, 2, 1, 2, 2, 2], [2, 1, 2, 2, 1, 3, 1], [2, 1, 2, 2, 2, 2, 1]]
-        scale = []
-        scale.push(root[1])
-        counter = root[0]
+        let scale = []
+        scale.push(root[0])
+        let counter = root[0]
         modes[category].forEach((mode)=> {
             counter += mode
-            console.log(counter)
-            scale.push(ChromaticScale[counter%12][1])
+            scale.push(ChromaticScale[counter%12][0])
         })
        
         return scale
@@ -28,9 +29,9 @@ function Fretboard() {
         let root = ChromaticScale[tonic]
         //  Major 7th - Minor 7th - Dominant 7th - Half Diminished 7th - Diminished 7th
         const notes = [[4, 3, 4, 1], [3, 4, 3, 2], [4, 3, 3, 2], [3, 3, 4, 2], [3, 3, 3, 3]]
-        arpeggio = []
+        let arpeggio = []
         arpeggio.push(root[0])
-        counter = root[0]
+        let counter = root[0]
         notes[category].forEach((note) => {
             counter += note
             arpeggio.push(ChromaticScale[counter%12][0])
@@ -38,10 +39,11 @@ function Fretboard() {
             
         return arpeggio
         }
+
     return (
         <div className="fretboard-grid">
             {/* Note the bloody round brackets on the map callback function below!!! */}
-            {notes.map((note) => (
+            {highlightedNotes.map((note) => (
                     <Fret className="fret" content={ChromaticScale[note][1]}/>
             ))}
         </div>
