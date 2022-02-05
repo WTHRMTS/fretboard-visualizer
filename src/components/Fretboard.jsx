c444732bf5adb150ec77bf3e8a2444689a18db8f
malediction99@gmail.com <malediction99@gmail.com>
2022-02-05 21:26:39 +1100

    fix intervals

--- src/components/Fretboard.jsx
+++ src/components/Fretboard.jsx
@@ -8,19 +8,21 @@ function Fretboard() {
     const ChromaticScale = [[0, 'C'], [1, 'C'+sharp + '/D'+flat], [2, 'D'], [3, 'D'+sharp+'/E'+flat], [4, 'E'], [5, 'F'], [6, 'F'+ sharp + '/G'+flat], [7, 'G'] ,
     [8, 'G'+ sharp + '/A' + flat], [9, 'A'], [10, 'A' + sharp + '/B'+flat], [11, 'B']];
 
-    let highlightedNotes = output_scale(5,0)
+    let highlightedNotes = output_scale(9,11)
 
     function output_scale(tonic, category) {
         let root = ChromaticScale[tonic]
-        //  Ionian - Dorian - Phrygian - Lydian - Mixolydian - Aeolian - Locrian - Harmonic Minor - Asc. Melodic Minor
-        const modes = [[2, 2, 1, 2, 2, 2, 1], [2, 1, 2, 2, 2, 1, 2], [1, 2, 2, 2, 1, 2, 2], [2, 2, 2, 1, 2, 2, 1], 
-        [2, 2, 1, 2, 2, 1, 2], [2, 1, 2, 2, 1, 2, 2], [1, 2, 2, 1, 2, 2, 2], [2, 1, 2, 2, 1, 3, 1], [2, 1, 2, 2, 2, 2, 1]]
+        // 0: Ionian - 1: Dorian - 2: Phrygian - 3: Lydian - 4: Mixolydian - 5: Aeolian - 6: Locrian - 7: Harmonic Minor
+        // 8: Asc. Melodic Minor - 9: Pentatonic Major - 10: Pentatonic Minor - 11: Blues Scale 
+        const scales = [[2, 4, 5, 7, 9, 11, 12], [2, 3, 5, 7, 9, 10, 12], [1, 3, 5, 7, 8, 10, 12], [2, 4, 6, 7, 9, 11, 12], 
+        [2, 4, 5, 7, 9, 10, 12], [2, 3, 5, 7, 8, 10, 12], [1, 3, 5, 6, 8, 10, 12], [2, 3, 5, 7, 8, 11, 12], 
+        [2, 3, 5, 7, 9, 11, 12], [2, 4, 7, 9, 12], [3, 5, 7, 10, 12], [3, 5, 6, 7, 10, 12]]
         let scale = []
         scale.push(root[0])
-        let counter = root[0]
-        modes[category].forEach((mode)=> {
-            counter += mode
-            scale.push(ChromaticScale[counter%12][0])
+        let start = root[0]
+        scales[category].forEach((interval) => {
+            let note = start + interval
+            scale.push(ChromaticScale[note%12][0])
         })
        
         return scale
@@ -28,13 +30,13 @@ function Fretboard() {
     function output_arpeggio(tonic, category) {
         let root = ChromaticScale[tonic]
         //  Major 7th - Minor 7th - Dominant 7th - Half Diminished 7th - Diminished 7th
-        const notes = [[4, 3, 4, 1], [3, 4, 3, 2], [4, 3, 3, 2], [3, 3, 4, 2], [3, 3, 3, 3]]
+        const arpeggios = [[4, 3, 4, 1], [3, 4, 3, 2], [4, 3, 3, 2], [3, 3, 4, 2], [3, 3, 3, 3]]
         let arpeggio = []
         arpeggio.push(root[0])
-        let counter = root[0]
-        notes[category].forEach((note) => {
-            counter += note
-            arpeggio.push(ChromaticScale[counter%12][0])
+        let start = root[0]
+        arpeggios[category].forEach((interval) => {
+            let note = start + interval
+            arpeggio.push(ChromaticScale[note%12][0])
         })
             
         return arpeggio
