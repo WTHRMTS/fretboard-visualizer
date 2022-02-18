5b3135e3cba13ad93ed2b8ebe6cc7fdd54624e07
malediction99@gmail.com <malediction99@gmail.com>
2022-02-18 12:37:49 +1100

    fix text

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -84,7 +84,7 @@ function App() {
                 in any key all over the fretboard.
                 </p>
                 <p>
-                    Hit the "Toggle Advanced Options" button to choose a 7-string or 8-string guitar fretboard and drop-tuning if required.
+                    Hit the "Toggle Advanced Options" button to choose a 7-string or 8-string guitar fretboard, detune the guitar up to 8 semitones, and use drop-tuning if required.
                 </p>
                 <p>
                 Example usage: select the C Major Scale, and run through the C Major 7, D minor 7, E minor 7, F Major 7, G Dominant 7, A minor 7 and B Half Diminished 7 arpeggios, 
